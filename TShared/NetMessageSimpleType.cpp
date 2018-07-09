0221f47b3dd1e7b5de8c25dd7852ec1bc464a79b
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2018-07-09 21:28:46 +0200

    Reset SImpleType to working version

--- TShared/NetMessageSimpleType.cpp
+++ TShared/NetMessageSimpleType.cpp
@@ -36,4 +36,7 @@ void CNetMessageSimpleType::Create(SSimpleTypeData aData)
 {
 	CNetMessage::Create(aData);
 	myData.myID = aData.myID;
+	myBool = aData.myBool;
+	myFloat = aData.myFloat;
+	myInt = aData.myInt;
 }
